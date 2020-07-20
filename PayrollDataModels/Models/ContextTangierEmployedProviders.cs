0b1f5d7c0a3ade7a64fda8c0b70d9d9998057626
Marrette, Lee <Lee_Marrette@tsged.com>
2020-07-20 16:36:16 -0500

    After updating the home page and adding processed payrolls and context

---
+++ PayrollDataModels/Models/ContextTangierEmployedProviders.cs
@@ -0,0 +1,21 @@
+﻿// Created By:           Lee Marrette
+//           Date:           07/20/2020
+//     Solution:           TangierEmployedProviders
+//       Project:           PayrollDataModels
+
+using System.Data.Entity;
+using TangierEmployedProviders.Models;
+
+namespace PayrollDataModels.Models
+{
+    public class ContextTangierEmployedProviders : DbContext
+    {
+        public DbSet<EmployeeAdditionalJob> EmployeeAdditionalJobs { get; set; }
+        public DbSet<EmployeeCommittment> EmployeeCommittments { get; set; }
+        public DbSet<EmployeeMaster> EmployeeMasters { get; set; }
+        public DbSet<GlobalIdMap> GlobalIdMaps { get; set; }
+        public DbSet<ProcessedPayroll> ProcessedPayrolls { get; set; }
+        public DbSet<SalesForceData> SalesForceDatas { get; set; }
+        public DbSet<StateTaxLegend> StateTaxLegends { get; set; }
+    }
+}
---
+++ PayrollDataModels/Models/ProcessedPayroll.cs
@@ -0,0 +1,46 @@
+﻿// Created By:           Lee Marrette
+//           Date:           04/14/2020
+//     Solution:           TangierICPay
+//       Project:           TangierICPay
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PayrollDataModels.Models
+{
+    /// <summary>
+    /// Table to show recent payroll loads and their status
+    /// </summary>
+    public class ProcessedPayroll
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// Date the data was loaded.
+        /// </summary>
+        [Column(TypeName="datetime2")]
+        public DateTime DateLoaded { get; set; }
+        /// <summary>
+        /// Earliest shift date
+        /// </summary>
+        [Column(TypeName="datetime2")]
+        public DateTime FromDate { get; set; }
+        /// <summary>
+        /// Ending shift date
+        /// </summary>
+        [Column(TypeName = "datetime2")]
+        public DateTime ThruDate { get; set; }
+        /// <summary>
+        /// Total dollar amount of the load
+        /// </summary>
+        public string TotalAmount { get; set; }
+        /// <summary>
+        /// Number of rows loaded
+        /// </summary>
+        public int RowCount { get; set; }
+        /// <summary>
+        /// Status of the data load
+        /// </summary>
+        public string Status { get; set; }
+    }
+}
